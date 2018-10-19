f0a55af3c9476a51472d2f2fab7a65b76bd1fc8c
zg <1181736238@qq.com>
2018-10-19 16:09:58 +0800

    Update

--- scripts/create-mysql.sh
+++ scripts/create-mysql.sh
@@ -9,6 +9,6 @@ EOF
 
 cp /root/.my.cnf /home/vagrant/.my.cnf
 
-DB=$1;
+# DB=$1;
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`$DB\` DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_unicode_ci";
+# mysql -e "CREATE DATABASE IF NOT EXISTS \`$DB\` DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_unicode_ci";
